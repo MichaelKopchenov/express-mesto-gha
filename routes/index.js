b5b31442024f7218b6d2b529d6ff503ac9a2f6f8
Mikhail Kopchenov <michaelkopchenov@yandex.ru>
2023-08-11 10:37:35 +0300

    ПР14, ревью №1

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
-const usersRouter = require('./users');
-const cardsRouter = require('./cards');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
-const { createUsers, login } = require('../controllers/users');
+const { createUser, login } = require('../controllers/users');
 
-router.use('/signup', createUsers);
-router.use('/signin', login);
+router.post('/signup', createUser);
+router.post('/signin', login);
+
 router.use(auth);
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
+
+router.use('/users', require('./users'));
+router.use('/cards', require('./cards'));
+
 router.use('*', (req, res, next) => {
   next(new NotFoundError('страница не найдена.'));
 });
