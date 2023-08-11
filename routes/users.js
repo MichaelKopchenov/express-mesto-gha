20998217107e623f93620929cc48440f49996269
Mikhail Kopchenov <michaelkopchenov@yandex.ru>
2023-08-11 23:24:48 +0300

    ПР14, tests

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const urlValidator = require('../utils/constants');
 const {
-  getUsers, getUserById, editDataOfUser, editDataOfUserAvatar, dataOfUser, login, createUser,
+  getUsers, getUserById, editDataOfUser, editDataOfUserAvatar, dataOfUser,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
@@ -27,22 +27,5 @@ router.patch('/me/avatar', celebrate({
     avatar: Joi.string().pattern(urlValidator),
   }),
 }), editDataOfUserAvatar);
-
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(3),
-  }),
-}), login);
-
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(urlValidator),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(3),
-  }).unknown(true),
-}), createUser);
 
 module.exports = router;
