4e36e0a7f5ccaabee5deb75568e42c68b9d3fc58
Mikhail Kopchenov <michaelkopchenov@yandex.ru>
2023-08-11 23:30:20 +0300

    ПР14, tests

--- routes/registration.js
+++ routes/registration.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { createUser } = require('../controllers/users');
-const urlRegex = require('../utils/constants');
+const urlValidator = require('../utils/constants');
 
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(urlRegex),
+    avatar: Joi.string().pattern(urlValidator),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(3),
   }).unknown(true),
